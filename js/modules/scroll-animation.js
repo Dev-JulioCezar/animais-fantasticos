0a47a56fb0c3ae572c79a3a20945bc1e2a91e257
Julio Cezar <dev.juliocezar@gmail.com>
2023-05-25 20:11:48 -0300

    scroll anima refeito

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -3,21 +3,44 @@ export default class AnimaScroll {
     this.section = document.querySelectorAll(section);
     this.windowPosition = window.innerHeight * 0.5;
 
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.section.forEach((item) => {
-      const itemTop = item.getBoundingClientRect().top;
-      if (itemTop < this.windowPosition) item.classList.add("ativo");
-      else if (item.classList.contains("ativo")) {
-        item.classList.remove("ativo");
+  // Pega a distância de cada item em relação ao topo
+  // do site
+  getDistance() {
+    this.distance = [...this.section].map((item) => {
+      const offset = item.offsetTop;
+      return {
+        element: item,
+        offset: Math.floor(offset - this.windowPosition),
+      };
+    });
+  }
+
+  // verifica a distancia em cada objeto
+  // em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add("ativo");
+      } else if (item.element.classList.contains("ativo")) {
+        item.element.classList.remove("ativo");
       }
     });
   }
 
   init() {
-    this.animaScroll();
-    window.addEventListener("scroll", this.animaScroll);
+    if (this.section.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
+    }
+    return this
+  }
+
+  // remove o event de scroll
+  stop(){
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
